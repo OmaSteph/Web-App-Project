f81181a9fe7d334981f0239387c139bb56191a5e
Oma <omastephanie25@gmail.com>
2025-03-04 10:12:47 +0100

    js update

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-let signupform = document.getElementsByClassName("form-control");
+//collecting data via the signup page
+// document.getElementById('signupForm').addEventListener('submit', function(register) {
+//     register.preventDefault ();
+    
+    // let message = 'hello world';
+    // console.log(message);
 
-signupform.addEventListener("submit", (e) => {
-    e.preventDefault();
+    // const name1 = document.getElementById('firstname');
 
-    let firstname = document.getElementById("firstname");
-    let lastname = document.getElementById("lastname");
-    let username = document.getElementById("email");
-    let password = document.getElementById("password");
-    let confirmPassword = document.getElementById("confirm-password");
+    const view = document.getElementById('#shop');
+    console.log({view})
 
-    alert("This form has been successfully submitted!");
-})
+    // console.log(name1)
+    // const name2 = document.getElementById('lastname').nodeValue;
+    // const username = document.getElementById('email').nodeValue;
+    // const password = document.getElementById('password').nodeValue;
+    // const confirmPassword = document.getElementById('confirmPassword').nodeValue;
+
+    // console.log(name1)
+    
+    // if (!name1 || !name2 || !username || !password || !confirmPassword) {
+    //     alert ('All fields are required');
+    //     return;
+    // }
+
+    // if (password.length < 8) {
+    //     alert('Passwword must be atleast 8 characters long');
+    //     return;
+    // }
+// });
